368d3f4cca18acbb2850131eb47106ba3d48bba3
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2026-09-18 10:23:25 +0200

    made settings public

--- pam_gamemode_voting/lua/pam/server/extensions/gamemode.lua
+++ pam_gamemode_voting/lua/pam/server/extensions/gamemode.lua
@@ -3,15 +3,15 @@ PAM_EXTENSION.name = name
 PAM_EXTENSION.enabled = true
 
 local setting_namespace = PAM.setting_namespace:AddChild(name)
-local vote_length_setting = setting_namespace:AddSetting("vote_length", pacoman.TYPE_INTEGER, 30, "The length of the gamemode voting time in seconds.")
-local blacklist_setting = setting_namespace:AddSetting("blacklist", pacoman.TYPE_STRING, "base", "Gamemodes that are listed here won't be available.")
+PAM_EXTENSION.vote_length_setting = setting_namespace:AddSetting("vote_length", pacoman.TYPE_INTEGER, 30, "The length of the gamemode voting time in seconds.")
+PAM_EXTENSION.blacklist_setting = setting_namespace:AddSetting("blacklist", pacoman.TYPE_STRING, "base", "Gamemodes that are listed here won't be available.")
 
 function PAM_EXTENSION:RegisterSpecialOptions()
 	if PAM.vote_type ~= "map" then return end
 
 	PAM.RegisterOption("change_gamemode", function()
 		PAM.Cancel()
-		PAM.Start("gamemode", vote_length_setting:GetActiveValue(), function(option)
+		PAM.Start("gamemode", self.vote_length_setting:GetActiveValue(), function(option)
 			PAM.ChangeGamemode(option)
 			PAM.Cancel()
 			PAM.Start()
@@ -25,7 +25,7 @@ function PAM_EXTENSION:RegisterOptions()
 	local all_gamemodes = engine.GetGamemodes()
 	for _, gamemode_table in ipairs(all_gamemodes) do
 		-- don't add blacklisted gamemodes
-		if string.find(blacklist_setting:GetActiveValue(), gamemode_table.name) then
+		if string.find(self.blacklist_setting:GetActiveValue(), gamemode_table.name) then
 			continue
 		end
 
